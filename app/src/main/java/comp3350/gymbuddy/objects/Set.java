d47516b6ea5e9dd82b5aed0a53c5815f78cf9e6e
Ty Paragas <ty.paragas01@gmail.com>
2025-02-09 17:24:32 -0600

    Populate Set.java

--- app/src/main/java/comp3350/gymbuddy/objects/Set.java
+++ app/src/main/java/comp3350/gymbuddy/objects/Set.java
@@ -1,4 +1,25 @@
 package comp3350.gymbuddy.objects;
 
 public class Set {
+    private Double weight;
+    private Integer repetitions;
+    private Double time;
+    private WorkoutItem associatedWorkoutItem;
+
+    public Set(Double weight, Integer repetitions, Double time, WorkoutItem associatedWorkoutItem){
+        this.weight = weight;
+        this.repetitions = repetitions;
+        this.time = time;
+        this.associatedWorkoutItem = associatedWorkoutItem;
+    }
+
+    public Double getWeight() { return this.weight; }
+    public Integer getRepetitions() { return this.repetitions; }
+    public Double getTime() { return this.time; }
+    public WorkoutItem getAssociatedWorkoutItem() { return this.associatedWorkoutItem; }
+
+    public void setWeight(Double weight) { this.weight = weight; }
+    public void setRepetitions(Integer repetitions) { this.repetitions = repetitions; }
+    public void setTime(Double time) { this.time = time; }
+    public void setAssociatedWorkoutItem(WorkoutItem associatedWorkoutItem) { this.associatedWorkoutItem = associatedWorkoutItem; }
 }
