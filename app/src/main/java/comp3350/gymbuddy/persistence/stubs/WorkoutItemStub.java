c84faa0db68b38d5c877017f601cf7cc78753004
Ty Paragas <ty.paragas01@gmail.com>
2025-02-09 17:43:45 -0600

    Add WorkoutItem stub DB and WorkoutItem JSON

---
+++ app/src/main/java/comp3350/gymbuddy/persistence/stubs/WorkoutItemStub.java
@@ -0,0 +1,52 @@
+package comp3350.gymbuddy.persistence.stubs;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import comp3350.gymbuddy.logic.ExerciseService;
+import comp3350.gymbuddy.objects.Exercise;
+import comp3350.gymbuddy.objects.WorkoutItem;
+
+public class WorkoutItemStub {
+    private List<WorkoutItem> workoutItems;
+
+    public WorkoutItemStub(){
+        this.workoutItems = new ArrayList<WorkoutItem>();
+
+        ExerciseService exService = new ExerciseService();
+        List<Exercise> exercises = exService.getAllExercises();
+
+        try{
+            String content = new String(Files.readAllBytes(Paths.get("workout-item.json")));
+            JSONArray workoutItemsJSON = new JSONArray(content);
+
+            for(int i=0; i<workoutItemsJSON.length(); i++){
+                JSONObject item = workoutItemsJSON.getJSONObject(i);
+
+                // look through exercises DB stub for corresponding exercise
+                // just so these workout items link to an existing exercise
+                String exerciseName = item.getString("Exercise Name");
+                Exercise toAdd = null;
+                for(Exercise e : exercises){
+                    if(e.getName().equalsIgnoreCase(exerciseName)){
+                        toAdd = e;
+                        break;
+                    }
+                }
+
+                Double weight = (item.getDouble("Weight") >= 0) ? (item.getDouble("Weight")) : null;
+                Integer repetitions = (item.getInt("Repetitions") >= 0) ? (item.getInt("Repetitions")) : null;
+                Double time = (item.getDouble("Time") >= 0) ? (item.getDouble("Time")) : null;
+
+                workoutItems.add(new WorkoutItem(toAdd, weight, repetitions, time));
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
