11823ba25fee557f95322aedd2a17792be8fa4e7
Farouk Amusat <amusatf@myumanitoba.ca>
2025-02-09 08:03:52 +0000

    Update file ExerciseService.java

--- app/src/main/java/comp3350/gymbuddy/logic/ExerciseService.java
+++ app/src/main/java/comp3350/gymbuddy/logic/ExerciseService.java
@@ -2,11 +2,16 @@ package comp3350.gymbuddy.business;
 
 import comp3350.gymbuddy.objects.Exercise;
 import comp3350.gymbuddy.persistence.IExercisePersistence;
-
+import comp3350.gymbuddy.persistence.stubs.ExerciseStub;
 import java.util.List;
 
 public class ExerciseService {
     private IExercisePersistence exercisePersistence;
+
+    // Default constructor uses ExerciseStub
+    public ExerciseService() {
+        this.exercisePersistence = new ExerciseStub(); // Default stub implementation
+    }
 
     public ExerciseService(IExercisePersistence exercisePersistence) {
         this.exercisePersistence = exercisePersistence;
