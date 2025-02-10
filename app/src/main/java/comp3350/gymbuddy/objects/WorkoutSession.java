8570fdcb84f630c93adee964e8e198374b385d5c
Ty Paragas <ty.paragas01@gmail.com>
2025-02-09 18:44:36 -0600

    Add WorkoutSessionStub, update WorkoutSession object

--- app/src/main/java/comp3350/gymbuddy/objects/WorkoutSession.java
+++ app/src/main/java/comp3350/gymbuddy/objects/WorkoutSession.java
@@ -3,19 +3,19 @@ package comp3350.gymbuddy.objects;
 import java.util.List;
 
 public class WorkoutSession {
-    private final int timestamp;
+    private final long timestamp;
     private final float duration;
     private final List<Set> sets;
     private final WorkoutProfile profile;
 
-    public WorkoutSession(int timestamp, float duration, List<Set> sets, WorkoutProfile profile) {
+    public WorkoutSession(long timestamp, float duration, List<Set> sets, WorkoutProfile profile) {
         this.timestamp = timestamp;
         this.duration = duration;
         this.sets = sets;
         this.profile = profile;
     }
 
-    public int getTimestamp() {
+    public long getTimestamp() {
         return timestamp;
     }
 
