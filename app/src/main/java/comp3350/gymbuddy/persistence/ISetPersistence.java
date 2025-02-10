91ea28f8aec16c138567857101c365f69c7fbe56
Ty Paragas <ty.paragas01@gmail.com>
2025-02-09 18:22:16 -0600

    Add Set stub database

---
+++ app/src/main/java/comp3350/gymbuddy/persistence/ISetPersistence.java
@@ -0,0 +1,9 @@
+package comp3350.gymbuddy.persistence;
+
+import java.util.List;
+
+import comp3350.gymbuddy.objects.Set;
+
+public interface ISetPersistence {
+    public List<Set> getAllSets();
+}
---
+++ app/src/main/java/comp3350/gymbuddy/persistence/stubs/SetStub.java
@@ -0,0 +1,39 @@
+package comp3350.gymbuddy.persistence.stubs;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+
+import comp3350.gymbuddy.logic.WorkoutItemService;
+import comp3350.gymbuddy.objects.Set;
+import comp3350.gymbuddy.objects.WorkoutItem;
+import comp3350.gymbuddy.persistence.ISetPersistence;
+
+public class SetStub implements ISetPersistence {
+    private List<Set> sets;
+
+    public SetStub(){
+        this.sets = new ArrayList<Set>();
+
+        WorkoutItemService itemService = new WorkoutItemService();
+        List<WorkoutItem> items = itemService.getAllWorkoutItems();
+
+        for(int i=0; i<items.size(); i++){
+            WorkoutItem wi = items.get(i);
+            int numSets = wi.getSets();
+
+            Random randNum = new Random();
+
+            for(int j=0; j<numSets; j++){
+                // random # of reps, varies by at most 1 from original # of reps
+                int randReps = randNum.nextInt(3) + wi.getRepetitions() - 1;
+
+                sets.add(new Set(wi.getWeight(), randReps, wi.getTime(), wi));
+            }
+        }
+    }
+
+    public List<Set> getAllSets(){
+        return new ArrayList<Set>(sets);
+    }
+}
