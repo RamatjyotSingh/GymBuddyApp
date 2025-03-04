ae4598c0b6abd157ca6e18f2c45bd193a8d24c9b
Logan Furedi <kolosso@protonmail.com>
2025-03-03 20:53:37 -0600

    refactor: replace FormValidator with exception handling
    * Add a bunch of custom exceptions for input validation
    * Modify WorkoutBuilderActivity and AddExerciseDialogFragment to use try catch blocks for invalid input handling.
    * Add InputValidator

---
+++ app/src/main/java/comp3350/gymbuddy/logic/InputValidator.java
@@ -0,0 +1,77 @@
+package comp3350.gymbuddy.logic;
+
+import java.util.List;
+
+import comp3350.gymbuddy.logic.exception.InvalidInputException;
+import comp3350.gymbuddy.logic.exception.InvalidNameException;
+import comp3350.gymbuddy.logic.exception.InvalidRepsException;
+import comp3350.gymbuddy.logic.exception.InvalidSetsException;
+import comp3350.gymbuddy.logic.exception.InvalidTimeException;
+import comp3350.gymbuddy.logic.exception.InvalidWeightException;
+import comp3350.gymbuddy.objects.Exercise;
+import comp3350.gymbuddy.objects.WorkoutItem;
+import comp3350.gymbuddy.objects.WorkoutProfile;
+
+public class InputValidator {
+    public WorkoutProfile newWorkoutProfile(String name, String iconPath, List<WorkoutItem> workoutItems) {
+        if (name.isEmpty()) {
+            throw new InvalidNameException("Name must be provided.");
+        }
+        if (workoutItems.isEmpty()) {
+            throw new InvalidInputException("Exercises must be provided.");
+        }
+
+        return new WorkoutProfile(name, iconPath, workoutItems);
+    }
+
+    public WorkoutItem newWorkoutItem(Exercise exercise, String setsField, String repsField, String weightField, String timeField) {
+        int sets;
+        int reps = 0;
+        double weight = 0.0;
+        double time = 0.0;
+
+        // Validate sets field.
+        try {
+            sets = Integer.parseInt(setsField);
+        } catch (NumberFormatException e) {
+            throw new InvalidSetsException("Must be a valid integer.");
+        }
+        if (sets < 1) {
+            throw new InvalidSetsException("Must be at least 1.");
+        }
+
+        if (exercise.isTimeBased()) {
+            // Validate time field.
+            try {
+                time = Double.parseDouble(timeField);
+            } catch (NumberFormatException e) {
+                throw new InvalidTimeException("Must be a valid number.");
+            }
+            if (time < 1.0) {
+                throw new InvalidTimeException("Must be at least 1.");
+            }
+        } else {
+            // Validate reps field.
+            try {
+                reps = Integer.parseInt(repsField);
+            } catch (NumberFormatException e) {
+                throw new InvalidRepsException("Must be a valid integer.");
+            }
+            if (reps < 1) {
+                throw new InvalidRepsException("Must be at least 1.");
+            }
+
+            if (exercise.hasWeight()) {
+                // Validate weight field.
+                try {
+                    weight = Double.parseDouble(weightField);
+                } catch (NumberFormatException e) {
+                    throw new InvalidWeightException("Must be a valid number.");
+                }
+            }
+        }
+
+        // Return the new workout item object.
+        return new WorkoutItem(exercise, sets, reps, weight, time);
+    }
+}
---
+++ app/src/main/java/comp3350/gymbuddy/logic/exception/InvalidRepsException.java
@@ -0,0 +1,7 @@
+package comp3350.gymbuddy.logic.exception;
+
+public class InvalidRepsException extends InvalidInputException {
+    public InvalidRepsException(String message) {
+        super(message);
+    }
+}
