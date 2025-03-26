2c4b1a44d3cebada093530dedfea3f3f9d41996a
Ty Paragas <paragast@myumanitoba.ca>
2025-03-26 13:15:01 -0500

    Fix broken unit test, add test suite for system tests

--- app/src/test/java/comp3350/gymbuddy/tests/logic/WorkoutManagerTest.java
+++ app/src/test/java/comp3350/gymbuddy/tests/logic/WorkoutManagerTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import comp3350.gymbuddy.logic.managers.WorkoutManager;
@@ -40,14 +41,18 @@ public class WorkoutManagerTest {
         List<WorkoutProfile> workoutProfiles;
         int beforeSize = workoutStub.getAll().size();
 
-        WorkoutProfile newProfile = new WorkoutProfile("Test Profile", "path", null);
+        WorkoutProfile newProfile = new WorkoutProfile("Test Profile", "path", new ArrayList<>());
 
         workoutManager.saveWorkout(newProfile);
 
         workoutProfiles = workoutStub.getAll();
 
         assertTrue(workoutStub.getAll().size() > beforeSize);
-        assertEquals(workoutProfiles.get(workoutProfiles.size()-1), newProfile);
+
+        WorkoutProfile addedProfile = workoutProfiles.get(workoutProfiles.size() - 1);
+        assertEquals(addedProfile.getName(), newProfile.getName());
+        assertEquals(addedProfile.getIconPath(), newProfile.getIconPath());
+        assertEquals(addedProfile.getWorkoutItems(), newProfile.getWorkoutItems());
     }
 
     @Test
