12be0ee56fe6e7eaec51417be13b9458201bd8f9
Logan Furedi <kolosso@protonmail.com>
2025-03-28 15:43:15 -0500

    style: improve formatting

--- app/src/main/java/comp3350/gymbuddy/presentation/activity/WorkoutLogActivity.java
+++ app/src/main/java/comp3350/gymbuddy/presentation/activity/WorkoutLogActivity.java
@@ -2,7 +2,6 @@ package comp3350.gymbuddy.presentation.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
